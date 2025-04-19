4e87932d161c0c7567de9fe9183d5b01698675b7
[toshibow] <ignitedrex@gmail.com>
2025-04-19 18:43:43 +0900

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Toppage from './pages/toppage'
 import Works from './pages/works'
 import About from './pages/about'
@@ -9,17 +9,16 @@ const App = () => {
   
 
   return (
-    <Router>
-    <Routes>
-      {/* Top Page */}
-      <Route path="/" element={<Toppage />} />
-      {/* works */}
-      <Route path="WORKS" element={<Works />} />
-      {/* about */}
-      <Route path="ABOUT" element={<About />} />
-   
-    </Routes>
-  </Router>
+    <HashRouter>
+      <Routes>
+        {/* Top Page */}
+        <Route path="/" element={<Toppage />} />
+        {/* works */}
+        <Route path="/WORKS" element={<Works />} />
+        {/* about */}
+        <Route path="/ABOUT" element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
